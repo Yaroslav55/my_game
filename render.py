732ea2260c11d915800658792c4fa1b4aa6d2340
Yaroslav55 <yarolavqaz@gmail.com>
2023-09-07 17:02:03 +0300

    Now all object(mesh) draw with VAO, VBO using specific shaders

--- render.py
+++ render.py
@@ -23,34 +23,60 @@ class Render(object):
         self._camera_obj = camera_obj
         self._game_scene = scene
         self._update_func = upd_func
+        self.default_vertexShaderSource = """
+                    #version 330 compatibility
+                    varying vec4 aPos;
+                    void main()
+                    {
+                        gl_TexCoord[0]=gl_MultiTexCoord0;
+                        aPos = gl_Vertex;
+                        //gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
+                        gl_Position = gl_ModelViewProjectionMatrix * aPos;
+                    }
+                    """
+        self.default_fragmentShaderSource = """
+                    #version 330 compatibility
+                    uniform sampler2D ourTexture;
+                    void main(void) {
+                        vec4 textureForPixel = texture2D(ourTexture, gl_TexCoord[0].st );
 
+                        gl_FragColor = textureForPixel;
+
+                    }
+                    """
         self.vertexShaderSource = """
-                    #version 330 core
+                    #version 330 compatibility
                     layout (location = 0) in vec3 aPos;
                     layout (location = 1) in vec3 aColor;
+                    layout (location = 2) in vec2 aTexCoord;
                     out vec3 ourColor;
-                        void main()
-                        {
-                            gl_Position = vec4(aPos, 1.0);
-                            ourColor = aColor;
+                    out vec2 TexCoord;
+                    void main()
+                    {
+                        gl_Position = gl_ModelViewProjectionMatrix * vec4(aPos, 1.0);
+                        ourColor = aColor;
+                        TexCoord = aTexCoord;
                     }"""
         self.fragmentShaderSource = """
                     #version 330 core
-                    out vec4 FragColor;
                     in vec3 ourColor;
+                    in vec2 TexCoord;
+                    out vec4 FragColor;
+                    uniform sampler2D ourTexture;
                         void main()
                         {
-                            FragColor = vec4(ourColor, 1.0f);
+                            FragColor = texture(ourTexture, TexCoord);
                         }
                     """
+        self.shaderProgram = None
         self.VAO = None
         self.VBO = None
-        self.vertices = np.array((
-            # positions       colors
-         0.5, -0.5, 0.0,   1.0, 0.0, 0.0,       # bottom right
-        -0.5, -0.5, 0.0,   0.0, 1.0, 0.0,       # bottom left
-         0.0,  0.5, 0.0,   0.0, 0.0, 1.0        # top
-        ), dtype='f')
+        self.EBO = None
+        self.texture = None
+        self.indices = np.array((0, 1, 3, 1, 2, 3 ), dtype=np.int32)
+        # self.indices = np.array((0, 0, 0,  # первый треугольник
+        #                         0  # второй треугольник
+        #                         ), dtype=np.int32)
     def run(self):
         self._opengl_init()
 
@@ -66,52 +92,74 @@ class Render(object):
             chunk.indexBuffer_ID.bind()
             glEnableVertexAttribArray(0)
 
+    def _compileShader(self, type, shader_source):
+        shader = glCreateShader(type)
+        glShaderSource(shader, shader_source)
+        glCompileShader(shader)
+        return shader
     # Процедура подготовки шейдера (тип шейдера, текст шейдера)
     def shader_init(self):
-        vertexShader = glCreateShader(GL_VERTEX_SHADER)
-        glShaderSource(vertexShader, self.vertexShaderSource)
-        glCompileShader(vertexShader)
+        vertexShader = self._compileShader(GL_VERTEX_SHADER, self.vertexShaderSource)
         success = glGetShaderiv(vertexShader, GL_COMPILE_STATUS)
         if not success:
             info_mesg = glGetShaderInfoLog(vertexShader)
             print("ERROR::SHADER::VERTEX::COMPILATION_FAILED\n", info_mesg)
         # fragment        shader
-        fragmentShader = glCreateShader(GL_FRAGMENT_SHADER)
-        glShaderSource(fragmentShader, self.fragmentShaderSource)
-        glCompileShader(fragmentShader)
+        fragmentShader = self._compileShader(GL_FRAGMENT_SHADER, self.fragmentShaderSource)
         # check for shader compile errors
         success = glGetShaderiv(fragmentShader, GL_COMPILE_STATUS)
         if not success:
             info_mesg = glGetShaderInfoLog(fragmentShader)
             print("ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n", info_mesg)
-        # link        shaders
 
-        shaderProgram = glCreateProgram()
-        glAttachShader(shaderProgram, vertexShader)
-        glAttachShader(shaderProgram, fragmentShader)
-        glLinkProgram(shaderProgram)
-        # check        for linking errors
-        success = glGetProgramiv(shaderProgram, GL_LINK_STATUS)
+        # link        shaders
+        self.shaderProgram = glCreateProgram()
+        glAttachShader(self.shaderProgram, vertexShader)
+        glAttachShader(self.shaderProgram, fragmentShader)
+        glLinkProgram(self.shaderProgram)
+        # check for linking errors
+        success = glGetProgramiv(self.shaderProgram, GL_LINK_STATUS)
         if not success:
-            info_mesg = glGetProgramInfoLog(shaderProgram)
+            info_mesg = glGetProgramInfoLog(self.shaderProgram)
             print( "ERROR::SHADER::PROGRAM::LINKING_FAILED\n", info_mesg )
         glDeleteShader(vertexShader)
         glDeleteShader(fragmentShader)
+        glUseProgram(self.shaderProgram)
+        def making_triangle():
+            vertices = np.array((
+                    # координаты        # цвета            // текстурные координаты
+                 0.5,  0.5, 0.0,   1.0, 0.0, 0.0,   1.0, 1.0, # верхняя правая вершина
+                 0.5, -0.5, 0.0,   0.0, 1.0, 0.0,   1.0, 0.0, # нижняя правая вершина
+                -0.5, -0.5, 0.0,   0.0, 0.0, 1.0,   0.0, 0.0, # нижняя левая вершина
+                -0.5,  0.5, 0.0,   1.0, 1.0, 0.0,   0.0, 1.0  # верхняя левая вершина
+            ), dtype='f')
+            # self.indices = np.array((0, 1, 3, # первый треугольник
+            #                     1, 2, 3  # второй треугольник
+            #                      ), dtype='f')
+            self.VAO = glGenVertexArrays(1)
+            self.VBO = glGenBuffers(1)
+            self.EBO = glGenBuffers(1)
+            float_size = 4
+            # bind the  Vertex Array Object first, then bind and set vertex  buffer(s), and then configure        vertex        attributes(s).
+            glBindVertexArray(self.VAO)
+            glBindBuffer(GL_ARRAY_BUFFER, self.VBO)
+            glBufferData(GL_ARRAY_BUFFER, len(vertices) * float_size, vertices, GL_STATIC_DRAW)
 
-        self.VAO = glGenVertexArrays(1)
-        self.VBO = glGenBuffers(1)
-        # bind        the        Vertex        Array        Object        first, then        bind and set        vertex        buffer(s), and then        configure        vertex        attributes(s).
-        glBindVertexArray(self.VAO)
-        glBindBuffer(GL_ARRAY_BUFFER, self.VBO)
-        glBufferData(GL_ARRAY_BUFFER, len(self.vertices) * 4, self.vertices, GL_STATIC_DRAW)
+            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, self.EBO)
+            glBufferData(GL_ELEMENT_ARRAY_BUFFER, len(self.indices) * 4, self.indices, GL_STATIC_DRAW)
 
-        # position        attribute
-        glVertexAttribPointer(0, 3, GL_FLOAT, False, 6 * 4, None)
-        glEnableVertexAttribArray(0)
-        # color        attribute
-        glVertexAttribPointer(1, 3, GL_FLOAT, False, 6 * 4, c_void_p(12))
-        glEnableVertexAttribArray(1)
-        glUseProgram(shaderProgram)
+            #mPosition = glGetAttribLocation(self.shaderProgram, "aPos") # Or 0
+            # position  attribute
+            glVertexAttribPointer(0, 3, GL_FLOAT, False, 8 * float_size, None)
+            glEnableVertexAttribArray(0)
+            # color     attribute
+            glVertexAttribPointer(1, 3, GL_FLOAT, False, 8 * float_size, c_void_p(3 * float_size))
+            glEnableVertexAttribArray(1)
+
+            glVertexAttribPointer(2, 2, GL_FLOAT, False, 8 * float_size, c_void_p(6 * float_size))
+            glEnableVertexAttribArray(2)
+
+        making_triangle()
 
 
     def _opengl_init(self):
@@ -217,58 +265,66 @@ class Render(object):
             return texture
 
         glBindTexture(GL_TEXTURE_2D, load_textureToGPU())
-        glVertexPointer(3, GL_FLOAT, 0, None)
-        """Draw a cube with texture coordinates"""
-        glBegin(GL_QUADS);
-        glTexCoord2f(0.0, 0.0);
-        glVertex3f(-1.0, -1.0, 1.0);
-        glTexCoord2f(1.0, 0.0);
-        glVertex3f(1.0, -1.0, 1.0);
-        glTexCoord2f(1.0, 1.0);
-        glVertex3f(1.0, 1.0, 1.0);
-        glTexCoord2f(0.0, 1.0);
-        glVertex3f(-1.0, 1.0, 1.0);
-        glTexCoord2f(1.0, 0.0);
-        glVertex3f(-1.0, -1.0, -1.0);
-        glTexCoord2f(1.0, 1.0);
-        glVertex3f(-1.0, 1.0, -1.0);
-        glTexCoord2f(0.0, 1.0);
-        glVertex3f(1.0, 1.0, -1.0);
-        glTexCoord2f(0.0, 0.0);
-        glVertex3f(1.0, -1.0, -1.0);
-        glTexCoord2f(0.0, 1.0);
-        glVertex3f(-1.0, 1.0, -1.0);
-        glTexCoord2f(0.0, 0.0);
-        glVertex3f(-1.0, 1.0, 1.0);
-        glTexCoord2f(1.0, 0.0);
-        glVertex3f(1.0, 1.0, 1.0);
-        glTexCoord2f(1.0, 1.0);
-        glVertex3f(1.0, 1.0, -1.0);
-        glTexCoord2f(1.0, 1.0);
-        glVertex3f(-1.0, -1.0, -1.0);
-        glTexCoord2f(0.0, 1.0);
-        glVertex3f(1.0, -1.0, -1.0);
-        glTexCoord2f(0.0, 0.0);
-        glVertex3f(1.0, -1.0, 1.0);
-        glTexCoord2f(1.0, 0.0);
-        glVertex3f(-1.0, -1.0, 1.0);
-        glTexCoord2f(1.0, 0.0);
-        glVertex3f(1.0, -1.0, -1.0);
-        glTexCoord2f(1.0, 1.0);
-        glVertex3f(1.0, 1.0, -1.0);
-        glTexCoord2f(0.0, 1.0);
-        glVertex3f(1.0, 1.0, 1.0);
-        glTexCoord2f(0.0, 0.0);
-        glVertex3f(1.0, -1.0, 1.0);
-        glTexCoord2f(0.0, 0.0);
-        glVertex3f(-1.0, -1.0, -1.0);
-        glTexCoord2f(1.0, 0.0);
-        glVertex3f(-1.0, -1.0, 1.0);
-        glTexCoord2f(1.0, 1.0);
-        glVertex3f(-1.0, 1.0, 1.0);
-        glTexCoord2f(0.0, 1.0);
-        glVertex3f(-1.0, 1.0, -1.0);
-        glEnd()
+        # glVertexPointer(3, GL_FLOAT, 0, None)
+        # """Draw a cube with texture coordinates"""
+        # texcoord_index = glGetAttribLocation(self.shaderProgram, "in_coord");
+        # glBegin(GL_QUADS);
+        # glTexCoord2f(0.0, 0.0);
+        # glVertex3f(-1.0, -1.0, 1.0);
+        # glTexCoord2f(1.0, 0.0);
+        #
+        # glVertex3f(1.0, -1.0, 1.0);
+        # glTexCoord2f(1.0, 1.0);
+        #
+        # glVertex3f(1.0, 1.0, 1.0);
+        #
+        # glTexCoord2f(0.0, 1.0);
+        # glVertex3f(-1.0, 1.0, 1.0);
+        # glTexCoord2f(1.0, 0.0);
+        # glVertex3f(-1.0, -1.0, -1.0);
+        #
+        # glTexCoord2f(1.0, 1.0);
+        # glVertex3f(-1.0, 1.0, -1.0);
+        #
+        # glTexCoord2f(0.0, 1.0);
+        # glVertex3f(1.0, 1.0, -1.0);
+        #
+        # glTexCoord2f(0.0, 0.0);
+        # glVertex3f(1.0, -1.0, -1.0);
+        #
+        # glTexCoord2f(0.0, 1.0);
+        # glVertex3f(-1.0, 1.0, -1.0);
+        # glTexCoord2f(0.0, 0.0);
+        # glVertex3f(-1.0, 1.0, 1.0);
+        # glTexCoord2f(1.0, 0.0);
+        # glVertex3f(1.0, 1.0, 1.0);
+        # glTexCoord2f(1.0, 1.0);
+        # glVertex3f(1.0, 1.0, -1.0);
+        # glTexCoord2f(1.0, 1.0);
+        # glVertex3f(-1.0, -1.0, -1.0);
+        # glTexCoord2f(0.0, 1.0);
+        # glVertex3f(1.0, -1.0, -1.0);
+        # glTexCoord2f(0.0, 0.0);
+        # glVertex3f(1.0, -1.0, 1.0);
+        # glTexCoord2f(1.0, 0.0);
+        # glVertex3f(-1.0, -1.0, 1.0);
+        # glTexCoord2f(1.0, 0.0);
+        # glVertex3f(1.0, -1.0, -1.0);
+        # glTexCoord2f(1.0, 1.0);
+        # glVertex3f(1.0, 1.0, -1.0);
+        # glTexCoord2f(0.0, 1.0);
+        # glVertex3f(1.0, 1.0, 1.0);
+        # glTexCoord2f(0.0, 0.0);
+        # glVertex3f(1.0, -1.0, 1.0);
+        # glTexCoord2f(0.0, 0.0);
+        # glVertex3f(-1.0, -1.0, -1.0);
+        # glTexCoord2f(1.0, 0.0);
+        # glVertex3f(-1.0, -1.0, 1.0);
+        # glTexCoord2f(1.0, 1.0);
+        # glVertex3f(-1.0, 1.0, 1.0);
+        # glTexCoord2f(0.0, 1.0);
+        # glVertex3f(-1.0, 1.0, -1.0);
+        # glEnd()
 
     def _DrawTerrain_with_VBO(self):
 
@@ -282,9 +338,8 @@ class Render(object):
         for chunk in self._game_scene.chunks:
             chunk.vertexBuffer_ID.bind()
             glVertexAttribPointer(0, 3, GL_FLOAT, False, 0, None)
-            # glDrawArrays(GL_TRIANGLES, 0, 3) #This line still works
-            glDrawElements(GL_TRIANGLE_STRIP, chunk.numb_of_triangles, GL_UNSIGNED_INT,
-                           None)  # This line does work too!
+            #glDrawArrays(GL_TRIANGLES, 0, 3) #This line still works
+            glDrawElements(GL_TRIANGLE_STRIP, chunk.numb_of_triangles, GL_UNSIGNED_INT,None)  # This line does work too!
 
     def VAO_enable(self):
         pass
@@ -298,14 +353,17 @@ class Render(object):
         self._make_camera(self._camera_obj.get_postion(), self._camera_obj.get_point_of_view())
         glScalef(1.0, 2.0, 1.0)
 
-        if 1:
+        if 0:
             glLineWidth(1)
             glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
         else:
             glPolygonMode(GL_FRONT_AND_BACK, GL_FILL)
+        if self.VAO != None:
+            self.drawCube()
+            glBindVertexArray(self.VAO)
+            glDrawElements(GL_TRIANGLE_STRIP, len(self.indices), GL_UNSIGNED_INT, None)
+            #glDrawArrays(GL_TRIANGLE_STRIP, 0, 6)
 
-        glBindVertexArray(self.VAO)
-        glDrawArrays(GL_TRIANGLES, 0, 3)
 
         # self.VAO_enable()
         #self.drawCube()
@@ -313,23 +371,28 @@ class Render(object):
         # glDeleteVertexArrays(1, [self.VAO])
         # glDeleteBuffers(1, [self.VBO])
         # glDeleteProgram(shaderProgram)
-        self._draw_terrain(const_var.TERRAIN_MODE)
-        # self._DrawTerrain_with_VBO()
+        #self._draw_terrain(const_var.TERRAIN_MODE)
+        #self._DrawTerrain_with_VBO()
 
         # glPolygonMode(GL_FRONT_AND_BACK, GL_FILL)
         # glColor3f(1.0, 0.0, 1.0)
         # glutSolidCube(0.05)
-        print("eee ")
-        glutSwapBuffers()
         #glFlush()
         delta_time = time.time() - delta_time  # The more the worse
+        glutSwapBuffers()
         # print("Delta time: ", delta_time)
 
     def opengl_error_check(self):
         error = glGetError()
         if error != GL_NO_ERROR:
             print("OPENGL_ERROR: ", gluErrorString(error))
-
+    def INfutureForDestrustor(self):
+        # Опционально: освобождаем        все        ресурсы, как        только
+        #  они        выполнили        свое        предназначение
+        # glDeleteVertexArrays(1, & VAO);
+        # glDeleteBuffers(1, & VBO);
+        # glDeleteBuffers(1, & EBO);
+        pass
     def Keyboard(self, key, x, y):
         cam_angel = self._camera_obj.cam_angel
         cam_angel_h = self._camera_obj.cam_angel_h
