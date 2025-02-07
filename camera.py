25288b83634a0fd236500881dfa9bacac52c6f3e
Yaroslav55 <yarolavqaz@gmail.com>
2025-02-07 13:58:53 +0200

    fix: bebug things

--- camera.py
+++ camera.py
@@ -22,8 +22,7 @@ class Camera3D(object):
     def move_back(self, angle = CAMERA_ANGEL, speed=CAMERA_SPEED):
         self._pos_x -= math.cos(self.cam_angel * math.pi / 180) * speed
         self._pos_z -= math.sin(self.cam_angel * math.pi / 180) * speed
-        self.player_mesh.directionOfMovement.set_variables(self._pos_x, self._pos_y, self._pos_z)
-        self.player_mesh.directionOfMovement.z += 30
+
     def move_up(self, speed=CAMERA_SPEED):
         self._pos_y += speed
 
