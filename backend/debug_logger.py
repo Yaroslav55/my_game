e4d19ff19a984a0a335b74a731deba09026fd3d4
Yaroslav55 <yarolavqaz@gmail.com>
2025-02-08 22:02:47 +0200

    feat(Logger): add exeptions

--- backend/debug_logger.py
+++ backend/debug_logger.py
@@ -2,11 +2,16 @@
 
 class Logger:
     info_header = "Debug info: "
-    info_header = "Debug warn: "
+    warn_header = "Debug warn: "
+    err_header = "Debug warn: "
     @classmethod
     def info(cls, msg):
         print(f"{cls.info_header}{msg}")
 
     @classmethod
     def warn(cls, msg):
-        print(f"{cls.warn_msg}{msg}")
+        print(f"{cls.warn_header}{msg}")
+
+    @classmethod
+    def err(cls, msg):
+        print(f"{cls.err_header}{msg}")
